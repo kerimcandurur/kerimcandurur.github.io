559ece626af2f7288908a7f3b1dc9b2a58344b79
kerimcandurur <kerimcan.durur33@gmail.com>
2022-01-07 09:29:45 +0300

    random song button

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-let songList = { "": "" };
+let songList = document.getElementsByTagName("a");
+
+function random() {
+    const random = Math.floor(Math.random() * songList.length);
+    console.log(random)
+    open(songList[random]);
+}
+
+
+//for (var i = 0, max = songList.length; i < max; i++) {}
