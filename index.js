8cc5ed395b75d73c54ff7e73c006afd7bafebf48
kerimcandurur <kerimcan.durur33@gmail.com>
2022-01-10 11:18:31 +0300

    broken search feature

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ let songList = document.getElementsByTagName("a");
 
 function random() {
     const random = Math.floor(Math.random() * songList.length);
-    console.log(random)
+    console.log(random);
     open(songList[random]);
 }
 
+function search() {
+    let input = document.getElementById("search").value;
+    input = input.toLowerCase();
+    let x = document.getElementsByTagName("li");
 
-//for (var i = 0, max = songList.length; i < max; i++) {}
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display = "none";
+        } else {
+            x[i].style.display = "list-item";
+        }
+    }
+}
