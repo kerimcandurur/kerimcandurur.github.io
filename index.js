0d8ca24c0ca05039aa4a266ff1f9205437efd380
Excalizan <aydinkement24@gmail.com>
2023-08-23 11:34:11 +0300

    format

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-let songList = document.getElementsByTagName("a");
+let songList = document.getElementsByTagName('a')
 
 function random() {
-    const random = Math.floor(Math.random() * songList.length);
-    console.log(random);
-    open(songList[random]);
+	const random = Math.floor(Math.random() * songList.length)
+	console.log(random)
+	open(songList[random])
 }
 
 function search() {
-    let input = document.getElementById("search").value;
-    input = input.toLowerCase();
-    let x = document.getElementsByTagName("ul");
+	let input = document.getElementById('search').value
+	input = input.toLowerCase()
+	let x = document.getElementsByTagName('ul')
 
-    for (i = 0; i < x.length; i++) {
-        if (!x[i].innerHTML.toLowerCase().includes(input)) {
-            x[i].style.display = "none";
-        } else {
-            x[i].style.display = "list-item";
-        }
-    }
+	for (i = 0; i < x.length; i++) {
+		if (!x[i].innerHTML.toLowerCase().includes(input)) {
+			x[i].style.display = 'none'
+		} else {
+			x[i].style.display = 'list-item'
+		}
+	}
 }
-
-
